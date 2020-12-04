602b5dce8b6629d51e8f9fecd8dc6a10fa357f98
brongus <Lewis_Whitear@outlook.com>
2020-12-04 00:06:49 +0000

    bootstrap

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
+// Environment config
+require("dotenv").config();
+const { PORT, MONGODB_URI } = process.env;
+
+// Dependencies
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
+const resourcesDir = "resources";
 
+// App setup
 const app = express();
 app.set("view engine", "ejs");
-require("dotenv").config();
+app.use(express.static(path.join(__dirname, resourcesDir)));
 
-const { PORT, MONGODB_URI } = process.env;
-
+// Database connection
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.on("error", (err) => {
-  console.error(err);
-  console.log(
-    "MongoDB connection error. Please make sure MongoDB is running.",
-    chalk.red("✗")
-  );
-  process.exit();
+    console.error(err);
+    console.log("MongoDB connection error. Please make sure MongoDB is running.");
+    process.exit();
 });
 
-app.use(express.static(path.join(__dirname, "resources")));
+// Routes
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
+});
+
+var fs = require('fs');
+var usersFilePath = path.join(__dirname, 'favicon.ico');
+app.get('/favicon.ico', function (req, res) {
+    res.send(usersFilePath);
 });
 
 app.listen(PORT, () => {
-  console.log(
-    `Example app listening at http://localhost:${PORT}`,
-    chalk.green("✓")
-  );
+    console.log("App listening at http://localhost:${PORT}");
 });
