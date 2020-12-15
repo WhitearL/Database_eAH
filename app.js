c2d3a373282574dcfdf4ed45e39eae989cca8e0d
brongus <Lewis_Whitear@outlook.com>
2020-12-15 21:05:17 +0000

    'test'

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const playerController = require("./controllers/player");
 
@@ -24,23 +24,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Database connection
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.on("error", (err) => {
-    console.error(err);
-    console.log("MongoDB connection error. Please make sure MongoDB is running.");
-    process.exit();
+  console.error(err);
+  console.log("MongoDB connection error. Please ensure MongoDB is running.");
+  process.exit();
 });
 
 // Routes
-app.get("/", (req, res) => { res.render("index"); });
-app.get("/browse", (req, res) => { res.render("browse"); });
-app.get("/manage", (req, res) => { res.render("manage"); });
-app.get("/analyse", (req, res) => { res.render("analyse"); });
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/browse", (req, res) => {
+  res.render("browse");
+});
+app.get("/manage", (req, res) => {
+  res.render("manage");
+});
+app.get("/analyse", (req, res) => {
+  res.render("analyse");
+});
 
-app.get("/createplayer", (req, res) => { res.render("createplayer"); });
+app.get("/createplayer", (req, res) => {
+  res.render("createplayer");
+});
 app.post("/createplayer", playerController.createPlayer);
 
 // Serve the page icon.
-var usersFilePath = path.join(__dirname, 'favicon.ico');
-app.get('/favicon.ico', function (req, res) { res.send(usersFilePath); });
+var usersFilePath = path.join(__dirname, "favicon.ico");
+app.get("/favicon.ico", function (req, res) {
+  res.send(usersFilePath);
+});
 
-// Listening
-app.listen(PORT, () => { console.log("App listening at http://localhost:${PORT}"); });
+// Listening.
+app.listen(PORT, () => {
+  console.log("App listening at http://localhost:${PORT}");
+});
